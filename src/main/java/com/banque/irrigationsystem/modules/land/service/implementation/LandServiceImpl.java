dd9b9d74e9261d8b98a1a545662b081d180005dd
owlytim <timothyowolabi@hellobrigit.com>
2022-12-10 03:37:56 +0100

    Implemented Service interfaces

---
+++ src/main/java/com/banque/irrigationsystem/modules/land/service/implementation/LandServiceImpl.java
@@ -0,0 +1,136 @@
+package com.banque.irrigationsystem.modules.land.service.implementation;
+
+import com.banque.irrigationsystem.core.exceptions.AppBaseException;
+import com.banque.irrigationsystem.modules.land.dto.AddLandRequest;
+import com.banque.irrigationsystem.modules.land.dto.LandDetail;
+import com.banque.irrigationsystem.modules.land.dto.PaginationRequest;
+import com.banque.irrigationsystem.modules.land.dto.UpdateLandRequest;
+import com.banque.irrigationsystem.modules.land.entity.LandEntity;
+import com.banque.irrigationsystem.modules.land.entity.dao.LandRepository;
+import com.banque.irrigationsystem.modules.land.service.LandService;
+import com.banque.irrigationsystem.shared.messaging.IrrigationEvent;
+import org.junit.platform.commons.util.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class LandServiceImpl implements LandService {
+
+    private LandRepository landRepository;
+
+    private ApplicationEventPublisher publisher;
+
+    @Autowired
+    public LandServiceImpl(LandRepository landRepository, ApplicationEventPublisher publisher) {
+        this.landRepository = landRepository;
+        this.publisher = publisher;
+    }
+
+    @Override
+    public LandDetail addLand(AddLandRequest addLandRequest) {
+
+        validateRequest(addLandRequest);
+
+        LandEntity landEntity = LandEntity.builder()
+                .landReference(addLandRequest.landReference())
+                .numberOfPlots(addLandRequest.numberOfPlots())
+                .description(addLandRequest.purpose())
+                .landType(addLandRequest.landType())
+                .build();
+
+        var recordId  = landRepository.save(landEntity).getId();
+
+        // push event to irrigation service to automatically create slot for land
+        publisher.publishEvent(new IrrigationEvent(landEntity.getLandType().name(),landEntity.getLandReference()));
+
+        return this.fetchLandById(recordId);
+    }
+
+    @Override
+    public LandDetail updateLand(UpdateLandRequest updateLandRequest) {
+
+        LandEntity landEntity = landRepository.findById(updateLandRequest.id())
+                .orElseThrow(()->new AppBaseException("Record not Found"));
+
+        Optional.ofNullable(updateLandRequest.numberOfPlots()).ifPresent(landEntity :: setNumberOfPlots);
+        Optional.ofNullable(updateLandRequest.landType()).ifPresent(landEntity :: setLandType);
+        Optional.ofNullable(updateLandRequest.purpose()).ifPresent(landEntity :: setDescription);
+
+        landRepository.save(landEntity);
+
+        return this.fetchLandById(landEntity.getId());
+
+    }
+
+    @Override
+    public List<LandDetail> fetchAllLands(PaginationRequest paginationRequest) {
+
+        Pageable pageable = Pageable.ofSize(paginationRequest.getPageSize());
+
+        return landRepository.findAll(pageable).stream()
+                .map(landEntity -> convertToLandDTO(landEntity))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public LandDetail fetchLandById(Long id) {
+        LandEntity landEntity = landRepository.findById(id).orElseThrow(()->new AppBaseException("Record not Found"));
+
+        return convertToLandDTO(landEntity);
+    }
+
+    @Override
+    public LandDetail fetchLandByReference(String reference) {
+        LandEntity landEntity =  landRepository.findByLandReference(reference).orElse(null);
+
+        return convertToLandDTO(landEntity);
+    }
+
+    @Override
+    public boolean landExists(String reference) {
+
+        LandEntity landEntity = landRepository.findByLandReference(reference).orElse(null);
+
+        if(Objects.isNull(landEntity)){
+            return false;
+        }
+
+        return true;
+    }
+
+
+    private LandDetail convertToLandDTO(LandEntity landEntity){
+
+        return new LandDetail(landEntity.getLandReference(),
+                landEntity.getNumberOfPlots(),
+                landEntity.getDescription(),
+                landEntity.getLandType(),
+                landEntity.getId(),
+                landEntity.getCreatedDate()
+                );
+    }
+
+
+    private void validateRequest(AddLandRequest addLandRequest){
+
+        if(addLandRequest.numberOfPlots() <= 0 ){
+            throw new AppBaseException("Invalid number of plots");
+        }
+
+        if(StringUtils.isBlank(addLandRequest.landReference())){
+            throw new AppBaseException("Land reference cannot be empty");
+        }
+
+        if(landExists(addLandRequest.landReference())){
+            throw new AppBaseException("Land already exists");
+        }
+
+    }
+}
---
+++ src/main/java/com/banque/irrigationsystem/modules/slot/service/implementation/IrrigationSlotServiceImpl.java
@@ -0,0 +1,174 @@
+package com.banque.irrigationsystem.modules.slot.service.implementation;
+
+import com.banque.irrigationsystem.modules.land.dto.LandDetail;
+import com.banque.irrigationsystem.modules.slot.dto.IrrigationStatus;
+import com.banque.irrigationsystem.modules.slot.dto.IrrigationType;
+import com.banque.irrigationsystem.modules.slot.dto.SlotConfiguration;
+import com.banque.irrigationsystem.modules.slot.entity.IrrigationSlot;
+import com.banque.irrigationsystem.modules.slot.entity.dao.IrrigationSlotRepository;
+import com.banque.irrigationsystem.modules.slot.exceptions.IrrigationServiceException;
+import com.banque.irrigationsystem.modules.slot.service.IrrigationSlotService;
+import com.banque.irrigationsystem.modules.land.dto.PaginationRequest;
+import com.banque.irrigationsystem.modules.land.service.LandService;
+import com.banque.irrigationsystem.shared.schedulers.Sensor;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.JobKey;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+@Slf4j
+@Service
+public class IrrigationSlotServiceImpl implements IrrigationSlotService {
+
+    private LandService landService;
+    private Scheduler scheduler;
+
+    private IrrigationSlotRepository slotRepository;
+
+    @Autowired
+    public IrrigationSlotServiceImpl(LandService landService, IrrigationSlotRepository slotRepository,Scheduler scheduler ) {
+        this.landService = landService;
+        this.slotRepository = slotRepository;
+        this.scheduler = scheduler;
+    }
+
+    @Override
+    public void configureSlot(String landReference, SlotConfiguration slotConfiguration) {
+
+        if(! landService.landExists(landReference)) {
+            throw new IrrigationServiceException("Land could not be found in irrigation channel");
+        }
+
+        IrrigationSlot slot = slotRepository.findByStatusAndLandReference(IrrigationStatus.PENDING,landReference)
+                .orElseThrow(()-> new IrrigationServiceException("No Pending Records"));
+
+        slot.setTimeToLaunch(slotConfiguration.timeToLaunch());
+        slot.setAmountOfWaterInGallons(slotConfiguration.amountOfWaterInGallons());
+        slot.setIrrigationIntervalInMinutes(slotConfiguration.irrigationIntervalInMinutes());
+        slotRepository.save(slot);
+        log.info("updated slot for land {}",landReference);
+
+
+        log.info("pushing updated slot to scheduler {}",slot.getJobId());
+
+
+        Map<String,String> dataMap = new HashMap<>();
+        dataMap.put("job_ref",slot.getJobId());
+
+        updateIrrigationSchedule(slot.getTimeToLaunch(),slot.getJobId(), dataMap);
+    }
+
+    @Override
+    public void createIrrigationSlot(String landReference) {
+
+        LandDetail detail = landService.fetchLandByReference(landReference);
+
+        IrrigationType irrigationToUse = IrrigationType.predictIrrigationSlotData(detail.landType());
+
+        IrrigationSlot irrigationSlot = IrrigationSlot.builder()
+                .landReference(landReference)
+                .irrigationType(irrigationToUse)
+                .amountOfWaterInGallons(irrigationToUse.getAmountOfWater())
+                .timeToLaunch(LocalDateTime.now().plusSeconds(irrigationToUse.getAfterTimeToTriggerInSeconds()))
+                .status(IrrigationStatus.PENDING)
+                .jobId(UUID.randomUUID().toString())
+                .build();
+
+        irrigationSlot = slotRepository.save(irrigationSlot);
+        log.info("created slot for land {}",landReference);
+
+        log.info("pushing slot to scheduler ID: {}",irrigationSlot.getJobId());
+        Map<String,String> dataMap = new HashMap<>();
+        dataMap.put("job_ref",irrigationSlot.getJobId());
+
+        scheduleIrrigation(irrigationSlot.getTimeToLaunch(),irrigationSlot.getJobId(), dataMap,true);
+    }
+
+    @Override
+    public Optional<IrrigationSlot> fetchSlotById(Long id) {
+        return slotRepository.findById(id);
+    }
+
+    @Override
+    public Optional<IrrigationSlot> fetchSlotByPendingLandRef(String landReference) {
+        return slotRepository.findByStatusAndLandReference(IrrigationStatus.PENDING,landReference);
+    }
+
+    @Override
+    public List<IrrigationSlot> fetchSlots(PaginationRequest paginationRequest) {
+
+        return slotRepository.findAll(Pageable.ofSize(paginationRequest.getPageSize()))
+                .getContent();
+    }
+
+    @Override
+    public void markSlotAsUtilized(String jobId) {
+        IrrigationSlot irrigationSlot = slotRepository.findByJobId(jobId);
+        irrigationSlot.setStatus(IrrigationStatus.COMPLETED);
+
+        slotRepository.save(irrigationSlot);
+        log.info("Slot status updated");
+    }
+
+
+    private String scheduleIrrigation(LocalDateTime triggerDateTime, String jobId, Map<String,String> jobData,boolean isNewJob) {
+
+        if(!isNewJob){
+            try {
+                scheduler.deleteJob(JobKey.jobKey(jobId));
+            } catch (SchedulerException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        //Set Job ID
+        JobBuilder builder = JobBuilder.newJob(Sensor.class)
+                .withIdentity(jobId,"slot_jobs");
+
+        //Set Job Data
+        jobData.entrySet().forEach(stringStringEntry -> {
+            builder.usingJobData(stringStringEntry.getKey(),stringStringEntry.getValue());
+        });
+
+        //Set job trigger
+        Trigger trigger = TriggerBuilder.newTrigger()
+                .startAt(Date.from(triggerDateTime.atZone(ZoneId.systemDefault()).toInstant()))
+                .build();
+
+        JobDetail job = builder.build();
+
+        //Schedule job
+        Set<Trigger>  triggers = new HashSet<>();
+        triggers.add(trigger);
+        try {
+            scheduler.scheduleJob(job, triggers, true);
+        } catch (SchedulerException e) {
+            throw new RuntimeException(e);
+        }
+
+        //Return job ID
+        return job.getKey().getName();
+    }
+
+    private void updateIrrigationSchedule(LocalDateTime triggerDateTime, String jobId, Map<String,String> jobData){
+        scheduleIrrigation(triggerDateTime,jobId,jobData,false);
+    }
+}
